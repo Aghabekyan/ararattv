3cbe555af728abe1a14c4674d3294130e55c5694
Sargis A <sargis@kinetics-sdm.com>
2017-08-17 17:37:12 +0400

    Collectstatic

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -74,13 +74,69 @@ $(function() {
 
 
 
+
+
+
+
+
+
+	function getCookie(name) {
+	    var cookieValue = null;
+	    if (document.cookie && document.cookie != '') {
+	        var cookies = document.cookie.split(';');
+	        for (var i = 0; i < cookies.length; i++) {
+	            var cookie = jQuery.trim(cookies[i]);
+	            // Does this cookie string begin with the name we want?
+	            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+	                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+	                break;
+	            }
+	        }
+	    }
+	    return cookieValue;
+	}
+	var csrftoken = getCookie('csrftoken');
+
+	function csrfSafeMethod(method) {
+	    // these HTTP methods do not require CSRF protection
+	    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	$.ajaxSetup({
+	    beforeSend: function(xhr, settings) {
+	        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+	            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+	        }
+	    }
+	});
+
+
+
+
+
+
+
+
 	/* setCookie */
+	var htmDIR = '/'
 	if ($('#make-count').length) {
 		var post_id = $('#make-count').data('id');
 		if (typeof getCookie(post_id) === "undefined" ) {
-			var url = htmDIR + '?hitcounter';
-			$.post(url, { 
-				id: post_id
+			var url = htmDIR + 'hitcounter';
+			$.ajax({
+			    type: 'POST',
+			    url: url,
+			    data: { 
+					id: post_id
+				},
+			    beforeSend: function(xhr, settings) {
+			        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+			            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			        }
+			    },
+			    success: function (data){
+			    	console.log(data)
+			    },
+			    error: function () {}
 			});
 			setCookie(post_id, 'default', 3);
 		}
